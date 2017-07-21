d38ac7ae410b7588750c9a35067ce6bf6d7c28ab
MichaeliaNa <nina.naliu1110@gmail.com>
2017-07-21 22:44:22 +0000

    separate css/js/html files

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const PORT_NUM = 8080;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname + '/templates/mymap.html'));
 });
+
+app.use('/js', express.static('js'));
+app.use('/css', express.static('css'));
 
 app.listen(PORT_NUM, function () {
   console.log('Example app listening on port ' + PORT_NUM + ' !');
